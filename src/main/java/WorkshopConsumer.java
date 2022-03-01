ea8188b96a3a344116b65fb5acefc483cfbe60a4
Pablo Estrada <pablof300@gmail.com>
2022-03-01 18:43:20 -0500

    Added starter code

---
+++ src/main/java/WorkshopConsumer.java
@@ -0,0 +1,10 @@
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.util.*;
+
+public class WorkshopConsumer {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/main/java/WorkshopProducer.java
@@ -0,0 +1,12 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
+import java.util.Properties;
+import java.util.concurrent.Future;
+
+public class WorkshopProducer {
+    public static void main(String[] args){
+
+    }
+}
